c1c8192d8fd6eaff82ba52f87c9b92a60d1291b1
quiliao-arthur <110624656+quiliao-arthur@users.noreply.github.com>
2023-03-15 17:06:43 -0400

    .

--- Lista3/resolucao/Equipe.java
+++ Lista3/resolucao/Equipe.java
@@ -3,4 +3,16 @@ package tlp120231.lista3.resolucao;
 public class Equipe{
     private Long id;
     private String nome;
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long novoId){
+        id=novoId;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String novoNome){
+        nome=novoNome;
+    }
 }
