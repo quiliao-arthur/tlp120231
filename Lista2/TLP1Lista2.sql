af024d5d0b3911a36078cf9931d21ca7d37fae4c
quiliao-arthur <110624656+quiliao-arthur@users.noreply.github.com>
2023-03-15 15:57:56 -0400

    .

--- Lista2/TLP1Lista2.sql
+++ Lista2/TLP1Lista2.sql
@@ -2,4 +2,13 @@
 SELECT professor.nome NomeProfessor,
  disciplina.nome DisciplinaQueMinistra
 FROM professor
- INNER JOIN
+ INNER JOIN disciplina ON professor.id = idprofessor;
+
+--b) Apresente o nome de todos os alunos cadastrados em ordem decrescente de idade.
+SELECT aluno.nome NomeAlunoPorIdadeDecrescente
+ FROM aluno
+ ORDER BY aluno.idade DESC;
+
+--c) Apresente o nome de todas as disciplinas cadastradas, o nome do curso que elas estão relacionadas bem como o nome do professor que ministra cada uma das disciplinas.
+SELECT disciplina.nome NomeDisciplina,
+ 
