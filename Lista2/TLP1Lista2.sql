b1a7d3203d0ace0498498240078d5d8e92de990d
quiliao-arthur <110624656+quiliao-arthur@users.noreply.github.com>
2023-03-15 16:34:47 -0400

    .

--- Lista2/TLP1Lista2.sql
+++ Lista2/TLP1Lista2.sql
@@ -11,4 +11,51 @@ SELECT aluno.nome NomeAlunoPorIdadeDecrescente
 
 --c) Apresente o nome de todas as disciplinas cadastradas, o nome do curso que elas estão relacionadas bem como o nome do professor que ministra cada uma das disciplinas.
 SELECT disciplina.nome NomeDisciplina,
+ curso.nome CursoDaDisciplina,
+ professor.nome ProfessorMinistrante
+FROM disciplina
+ INNER JOIN professor ON idprofessor = professor.id
+ INNER JOIN disciplinacurso ON disciplina.id=iddisciplina
+ INNER JOIN curso ON idcurso=curso.id;
+
+--d) Apresente o nome de todos os professores que possuem doutorado e o nome da universidade que eles trabalham.
+SELECT professor.nome ProfessoresDoutores,
+ universidade.nome UniversidadeAtuacao
+FROM professor
+ INNER JOIN universidade ON iduniversidade=universidade.id
+WHERE professor.titulacao = 'Doutorado';
+
+--e) Apresente o nome das universidades que possuem professores que atuam na área de programação de computadores, que possuem mestrado e que ministram alguma disciplina com carga horária superior a 40 horas.
+SELECT DISTINCT universidade.nome 
+ Universidade_ProfessoresMestresProgramaçãoComputadores_DisciplinaMaisde40H
+FROM universidade
+ INNER JOIN professor ON universidade.id=iduniversidade
+ INNER JOIN disciplina ON professor.id=idprofessor
+WHERE professor.areadeatuacao = 'Programação de Computadores'
+ AND professor.titulacao = 'Mestrado'
+ AND disciplina.cargahoraria>40;
+
+--f) Apresente o nome de todos os cursos que possuem disciplinas ministradas por professores com mestrado que atuam na área de matemática. O curso apresenteado deve ter algum aluno do sexo masculino.
+SELECT DISTINCT curso.nome
+ Curso_ProfessorMestreMatemática_AlunoMasculino
+FROM curso 
+ INNER JOIN aluno ON curso.id=aluno.idcurso
+ INNER JOIN disciplinacurso ON curso.id=disciplinacurso.idcurso
+ INNER JOIN disciplina ON iddisciplina=disciplina.id
+ INNER JOIN professor ON idprofessor=professor.id
+WHERE professor.titulacao = 'Mestrado'
+ AND professor.areadeatuacao = 'Matemática'
+ AND aluno.sexo = 'Masculino';
+
+--g) Apresente o nome das universidades que possuem professores que ministram a disciplina de ética, com carga horária de 20 horas e que está vinculada ao curso de engenharia de computação.
+SELECT universidade.nome
+ Universidade_DisciplinaÉticaCarga=20h_CursoEngenhariaDeComputação
+FROM universidade
+ INNER JOIN professor ON universidade.id=iduniversidade
+ INNER JOIN disciplina ON professor.id=idprofessor
+ INNER JOIN disciplinacurso ON disciplina.id=iddisciplina
+ INNER JOIN curso ON idcurso=curso.id
+WHERE disciplina.nome='Ética'
+ AND disciplina.cargahoraria=20
+ AND curso.nome='Engenharia de computação';
  
