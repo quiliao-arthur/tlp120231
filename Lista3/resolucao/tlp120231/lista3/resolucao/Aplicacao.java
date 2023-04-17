249747b4bfe3a75231430066c4c3c66a4dd2d9b8
Arthur Quilião <arthurq2m@gmail.com>
2023-04-17 12:41:27 -0400

    .

---
+++ Lista3/resolucao/tlp120231/lista3/resolucao/Aplicacao.java
@@ -0,0 +1,95 @@
+package tlp120231.lista3.resolucao;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Aplicacao {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int escolha = -1;
+        ArrayList<Lutadora> listaLutadoras = new ArrayList<Lutadora>();
+        ArrayList<Competidora> listaCompetidora = new ArrayList<Competidora>();
+        Lutadora lutadora = new Lutadora();
+        do{
+
+            System.out.println("Sistema de Gerenciamento de Atletas e Competições:\n"+
+                                "1-) Cadastro de Atletas\n"+
+                                "2-) Atualização de status para afastamento\n"+
+                                "3-) Exibição de lutas\n"+
+                                "4-) Premiação\n"+
+                                "5-) Exibição de ranking de competidoras\n"+
+                                "6-) Atleta sem competição\n"+
+                                "0-) Sair");
+
+            switch(escolha=(int) validaNumero(sc)){
+
+                case -1337:{
+                    System.out.println("Algo deu muito errado.");
+                    break;
+                }
+
+                case 0:{
+                    break;
+                }
+
+                case 1:{
+                    System.out.println("Cadastro de Atleta!\n" +
+                                        "Nome da atleta:");
+                    lutadora.setNome(sc.nextLine());
+                    System.out.println("Idade da atleta:");
+                    lutadora.setIdade((int) validaNumero(sc));
+                    System.out.println("Peso(kg) da atleta:");
+                    lutadora.setPeso_kg(validaNumero(sc));
+                    System.out.println("Altura(cm) da atleta:");
+                    lutadora.setAltura_cm(validaNumero(sc));
+                    System.out.println("Equipe da atleta:");
+                    lutadora.setEquipe(sc.nextLine());
+                    listaLutadoras.add(lutadora);
+                    break;
+                }
+
+                case 2:{
+                    System.out.println("Atualização de status para afastamento\n"+
+                                        "Nome da atleta afastada:");
+                    break;
+                }
+
+                case 3:{
+                    break;
+                }
+
+                case 4:{
+                    break;
+                }
+
+                case 5:{
+                    break;
+                }
+
+                case 6:{
+                    break;
+                }
+
+                default:{
+                    System.out.println("Favor escolher um número de 0 a 6;");
+                    break;
+                }
+            }
+        }while(escolha!=0);
+    }
+
+    public static double validaNumero(Scanner sc){
+        boolean erro = false;
+        do{
+            try{
+                return Double.parseDouble(sc.nextLine());
+            }catch(Exception e){
+                erro=true;
+                System.out.println("Favor inserir um número!");
+            }
+        }while(erro);
+        return (double) -1337;
+    }
+
+    
+}
---
+++ Lista3/resolucao/tlp120231/lista3/resolucao/Lutadora.java
@@ -0,0 +1,53 @@
+package tlp120231.lista3.resolucao;
+
+public class Lutadora {
+    
+    private String nome;
+
+    private int idade;
+
+    private double peso_kg;
+
+    private double altura_cm;
+
+    private String equipe;
+
+    public String getNome(){return nome;}
+
+    public void setNome(String nome){this.nome=nome;}
+
+    public int getIdade(){return idade;}
+
+    public void setIdade(int idade){this.idade=idade;}
+
+    public double getPeso_kg(){return peso_kg;}
+
+    public void setPeso_kg(double peso_kg){this.peso_kg=peso_kg;}
+
+    public double getAltura_cm(){return altura_cm;}
+    
+    public void setAltura_cm(double altura_cm){this.altura_cm=altura_cm;}
+
+    public String getEquipe(){return equipe;}
+
+    public void setEquipe(String equipe){this.equipe=equipe;}
+
+    public Lutadora(String nome, int idade, double peso_kg, double altura_cm, String equipe){
+        super();
+        this.nome=nome; this.idade=idade; this.peso_kg=peso_kg; this.altura_cm=altura_cm;
+        this.equipe=equipe;
+    }
+
+    public Lutadora(Lutadora lutadora){
+        super();
+        this.nome=lutadora.getNome(); this.idade=lutadora.getIdade(); 
+        this.peso_kg=lutadora.getPeso_kg(); this.altura_cm=lutadora.getAltura_cm(); 
+        this.equipe=lutadora.getEquipe();
+    }
+
+    public Lutadora(){super();}
+
+    public String toString(){
+        return "Atleta:\n-Nome: "+nome+"\n"
+    }
+}
